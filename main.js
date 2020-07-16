f2db9541a4c9d0a9710c00deb5926bb5dcedc078
seragelgamal <55516965+seragelgamal@users.noreply.github.com>
2020-07-16 13:08:57 -0600

    commit

--- main.js
+++ main.js
@@ -21,157 +21,133 @@ let boardArray = [
     [2, 3, 4, 5, 6, 4, 3, 2]
 ];
 
-let numberLegend = ['', 'Pawn', 'Rook', 'Knight', 'Bishop', 'Queen', 'King'];
+let numberLegend = ['Empty', 'Pawn', 'Rook', 'Knight', 'Bishop', 'Queen', 'King'];
 
-let blackPieces = {
-    pawn1: {
-        row: 1,
-        column: 0,
-        moved: false
-    },
-    pawn2: {
-        row: 1,
-        column: 1,
-        moved: false
-    },
-    pawn3: {
-        row: 1,
-        column: 2,
-        moved: false
-    },
-    pawn4: {
-        row: 1,
-        column: 3,
-        moved: false
-    },
-    pawn5: {
-        row: 1,
-        column: 4,
-        moved: false
-    },
-    pawn6: {
-        row: 1,
-        column: 5,
-        moved: false
-    },
-    pawn7: {
-        row: 1,
-        column: 6,
-        moved: false
-    },
-    pawn8: {
-        row: 1,
-        column: 7,
-        moved: false
-    },
-    rook1: {
-        row: 0,
-        column: 0
-    },
-    rook2: {
-        row: 0,
-        column: 7
-    },
-    knight1: {
-        row: 0,
-        column: 1
-    },
-    knight2: {
-        row: 0,
-        column: 6
-    },
-    bishop1: {
-        row: 0,
-        column: 2
-    },
-    bishop2: {
-        row: 0,
-        column: 5
-    },
-    queen: {
-        row: 0,
-        column: 3
-    },
-    king: {
-        row: 0,
-        column: 4
-    }
-}
+let blackPawns = [{
+    row: 1,
+    column: 0,
+    moved: false
+}, {
+    row: 1,
+    column: 1,
+    moved: false
+}, {
+    row: 1,
+    column: 2,
+    moved: false
+}, {
+    row: 1,
+    column: 2,
+    moved: false
+}, {
+    row: 1,
+    column: 4,
+    moved: false
+}, {
+    row: 1,
+    column: 5,
+    moved: false
+}, {
+    row: 1,
+    column: 6,
+    moved: false
+}, {
+    row: 1,
+    column: 7,
+    moved: false
+}];
+let blackRooks = [{
+    row: 0,
+    column: 0
+}, {
+    row: 0,
+    column: 7
+}];
+let blackKnights = [{
+    row: 0,
+    column: 1
+}, {
+    row: 0,
+    column: 6
+}];
+let blackBishops = [{
+    row: 0,
+    column: 2
+}, {
+    row: 0,
+    column: 5
+}];
+let blackQueen = {
+    row: 0,
+    column: 3
+};
+let blackKing = {
+    row: 0,
+    column: 4
+};
 
-let whitePieces = {
-    pawn1: {
-        row: 6,
-        column: 0,
-        moved: false
-    },
-    pawn2: {
-        row: 6,
-        column: 1,
-        moved: false
-    },
-    pawn3: {
-        row: 6,
-        column: 2,
-        moved: false
-    },
-    pawn4: {
-        row: 6,
-        column: 3,
-        moved: false
-    },
-    pawn5: {
-        row: 6,
-        column: 4,
-        moved: false
-    },
-    pawn6: {
-        row: 6,
-        column: 5,
-        moved: false
-    },
-    pawn7: {
-        row: 6,
-        column: 6,
-        moved: false
-    },
-    pawn8: {
-        row: 6,
-        column: 7,
-        moved: false
-    },
-    rook1: {
-        row: 7,
-        column: 0
-    },
-    rook2: {
-        row: 7,
-        column: 7
-    },
-    knight1: {
-        row: 7,
-        column: 1
-    },
-    knight2: {
-        row: 7,
-        column: 6
-    },
-    bishop1: {
-        row: 7,
-        column: 2
-    },
-    bishop2: {
-        row: 7,
-        column: 5
-    },
-    queen: {
-        row: 7,
-        column: 3
-    },
-    king: {
-        row: 7,
-        column: 4
-    }
-}
+let whitePawns = [{
+    row: 6,
+    column: 0,
+    moved: false
+}, {
+    row: 6,
+    column: 1,
+    moved: false
+}, {
+    row: 6,
+    column: 2,
+    moved: false
+}, {
+    row: 6,
+    column: 2,
+    moved: false
+}, {
+    row: 6,
+    column: 4,
+    moved: false
+}, {
+    row: 6,
+    column: 5,
+    moved: false
+}, {
+    row: 6,
+    column: 6,
+    moved: false
+}, {
+    row: 6,
+    column: 7,
+    moved: false
+}];
+let whiteRooks = [{
+    row: 7,
+    column: 0
+}, {
+    row: 7,
+    column: 7
+}];
+let whiteKnights = [{
+    row: 7,
+    column: 1
+}, {
+    row: 7,
+    column: 6
+}];
+let whiteBishops = [{
+    row: 7,
+    column: 2
+}, {
+    row: 7,
+    column: 5
+}];
+let whiteQueen = {
+    row: 7,
+    column: 3
+};
+let whiteKing = {
+    row: 7,
+    column: 4
+};
 
 for (let row = 0; row < 8; row++) {
     for (let square = 0; square < 8; square++) {
@@ -180,7 +156,7 @@ for (let row = 0; row < 8; row++) {
         whiteSquare.id = row + '-' + square;
         whiteSquare.dataset.row = row;
         whiteSquare.dataset.column = square;
-        whiteSquare.dataset.piece = 'white' + numberLegend[boardArray[row][square]];
+        whiteSquare.dataset.piece = numberLegend[boardArray[row][square]];
         boardContainer.append(whiteSquare);
 
         square++;
@@ -190,7 +166,7 @@ for (let row = 0; row < 8; row++) {
         brownSquare.id = row + '-' + square;
         brownSquare.dataset.row = row;
         brownSquare.dataset.column = square;
-        brownSquare.dataset.piece = 'black' + numberLegend[boardArray[row][square]];
+        brownSquare.dataset.piece = numberLegend[boardArray[row][square]];
         boardContainer.append(brownSquare);
     }
     row++;
@@ -200,7 +176,7 @@ for (let row = 0; row < 8; row++) {
         brownSquare.id = row + '-' + square;
         brownSquare.dataset.row = row;
         brownSquare.dataset.column = square;
-        brownSquare.dataset.piece = 'black' + numberLegend[boardArray[row][square]];
+        brownSquare.dataset.piece = numberLegend[boardArray[row][square]];
         boardContainer.append(brownSquare);
 
         square++;
@@ -210,25 +186,43 @@ for (let row = 0; row < 8; row++) {
         whiteSquare.id = row + '-' + square;
         whiteSquare.dataset.row = row;
         whiteSquare.dataset.column = square;
-        whiteSquare.dataset.piece = 'white' + numberLegend[boardArray[row][square]];
+        whiteSquare.dataset.piece = numberLegend[boardArray[row][square]];
         boardContainer.append(whiteSquare);
     }
 }
 
 for (let row = 0; row < 2; row++) {
     for (let column = 0; column < 8; column++) {
-        if (numberLegend[boardArray[row][column]] != 0) {
-            document.getElementById(row + '-' + column).innerHTML += '<img src="media/black' + numberLegend[boardArray[row][column]] + '.png"></img>';
+        document.getElementById(row + '-' + column).innerHTML += '<img src="media/black' + numberLegend[boardArray[row][column]] + '.png"></img>';
+        document.getElementById(row + '-' + column).dataset.pieceColour = 'black';
+        if (row == 1) {
+            document.getElementById(row + '-' + column).dataset.number = column;
         }
     }
 }
 for (let row = 6; row < 8; row++) {
     for (let column = 0; column < 8; column++) {
-        if (numberLegend[boardArray[row][column]] != 0) {
-            document.getElementById(row + '-' + column).innerHTML += '<img src="media/white' + numberLegend[boardArray[row][column]] + '.png"></img>';
+        document.getElementById(row + '-' + column).innerHTML += '<img src="media/white' + numberLegend[boardArray[row][column]] + '.png"></img>';
+        document.getElementById(row + '-' + column).dataset.pieceColour = 'white';
+        if (row == 6) {
+            document.getElementById(row + '-' + column).dataset.number = column;
         }
     }
 }
+
+document.getElementById('0-0').dataset.number = 0;
+document.getElementById('0-7').dataset.number = 1;
+document.getElementById('0-1').dataset.number = 0;
+document.getElementById('0-6').dataset.number = 1;
+document.getElementById('0-2').dataset.number = 0;
+document.getElementById('0-5').dataset.number = 1;
+
+document.getElementById('7-0').dataset.number = 0;
+document.getElementById('7-7').dataset.number = 1;
+document.getElementById('7-1').dataset.number = 0;
+document.getElementById('7-6').dataset.number = 1;
+document.getElementById('7-2').dataset.number = 0;
+document.getElementById('7-5').dataset.number = 1;
 
 let whiteTurn = true;
 
@@ -239,17 +233,28 @@ for (let i = 0; i < squareElements.length; i++) {
     squareElements[i].addEventListener('mouseleave', mouseLeaveHandler);
 }
 
-function mouseEnterHandler(e) {
+function mouseEnterHandler(event) {
     // console.log(e.target);
     if (whiteTurn) {
-        if (e.target.dataset.piece == 'Pawn' && (!whitePieces.this.moved)) {
-
-        } 
+        if (event.target.dataset.piece == 'Pawn' && event.target.dataset.pieceColour == 'white' && !whitePawns[event.target.dataset.number].moved) {
+            highlightPossibleMoves(event, 'unmoved' + event.target.dataset.piece);
+        }
     } else {
 
     }
 }
 
-function mouseLeaveHandler(e) {
-    // console.log(e);
+function mouseLeaveHandler(event) {
+    for (let i = 0; i < squareElements.length; i++) {
+        squareElements[i].classList.remove('highlighted');
+    }
+}
+
+function highlightPossibleMoves(event, piece) {
+    if (piece == 'unmovedPawn') {
+        for (let row = event.target.dataset.row; row > event.target.dataset.row - 2;) {
+            row--;
+            document.getElementById(row + '-' + event.target.dataset.column).classList.add('highlighted');
+        }
+    }
 }
